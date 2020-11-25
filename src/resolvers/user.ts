4c694dfd1f3c66bd6f74d7f8aaf634a463688d2d
viniciuskmv <viniciuskmv@gmail.com>
2020-11-25 16:59:40 -0300

    feat: ensure cookies are set when registering/logging

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -93,7 +93,7 @@ export class UserResolver {
   @Mutation(() => UserResponse)
   async login(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username });
     if (!user) {
@@ -107,6 +107,9 @@ export class UserResolver {
         errors: [{ field: "password", message: "Incorrect password" }],
       };
     }
+
+    req.session.userId = user.id;
+
     return { user };
   }
 }
