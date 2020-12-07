bbdc96517093b7f7c4cc86a188257f1bd481f16b
viniciuskmv <viniciuskmv@gmail.com>
2020-12-07 11:41:27 -0300

    chore: fix typo

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ const main = async () => {
   const app = express();
   const RedisStore = connectRedis(session);
   const redis = new Redis(process.env.REDIS_URL);
-  app.set("proxy", 1);
+  app.set("trust proxy", 1);
 
   app.use(
     cors({
